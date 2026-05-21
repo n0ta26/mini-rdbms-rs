58e2d4abd3c91f190eae17459f61ff49f4165803
n0ta <teroro1226@outlook.jp>
2026-05-21 13:34:16 +0900

    refactor: reuse shared TestFile in integration tests

--- tests/access_to_storage_integration_test.rs
+++ tests/access_to_storage_integration_test.rs
@@ -3,52 +3,16 @@ use mini_rdbms_rs::buffer::LruBufferManager;
 use mini_rdbms_rs::disk::{PageId, StorageDiskManager};
 use mini_rdbms_rs::page::{PageError, SlottedPageManager};
 use mini_rdbms_rs::storage::FileStorage;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::process;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::path::Path;
+
+#[path = "../src/test_utils.rs"]
+mod test_utils;
+use test_utils::TestFile;
 
 type TestDiskManager = StorageDiskManager<FileStorage>;
 type TestBufferManager = LruBufferManager<TestDiskManager>;
 type TestAccessMethod =
     HeapAccessMethod<BufferPoolPageAccessor<TestBufferManager>, SlottedPageManager>;
-
-struct TestFile {
-    path: PathBuf,
-}
-
-impl TestFile {
-    fn new(test_name: &str) -> Self {
-        static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
-
-        let unique_id = NEXT_ID.fetch_add(1, Ordering::Relaxed);
-        let timestamp = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("system time should be after unix epoch")
-            .as_nanos();
-        let file_name = format!(
-            "mini_rdbms_{test_name}_{}_{}_{}.db",
-            process::id(),
-            timestamp,
-            unique_id
-        );
-
-        Self {
-            path: std::env::temp_dir().join(file_name),
-        }
-    }
-
-    fn path(&self) -> &Path {
-        &self.path
-    }
-}
-
-impl Drop for TestFile {
-    fn drop(&mut self) {
-        let _ = fs::remove_file(&self.path);
-    }
-}
 
 fn create_access_method(
     path: &Path,
